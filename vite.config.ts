d18d6b48abec93b8abcc25e9651633b54418c49c
Kimo <elmergawy96@gmail.com>
2025-09-06 18:43:10 -0700

    Configure GitHub Pages Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/elitefinder/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
